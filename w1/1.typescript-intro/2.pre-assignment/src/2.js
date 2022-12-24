bc59813da4565a97af7899dee1047204a944d7db
agrawalrajat310 <agrawalrajat03@outlook.com>
2022-12-25 01:29:12 +0530

    day - 1 assign push

---
+++ w1/1.typescript-intro/2.pre-assignment/src/2.js
@@ -0,0 +1,4 @@
+// 2. Create a type age with a number ?
+var age = 51;
+age = 89; // success (okay)
+age = "fifty one"; // error (not okay)
--- w1/1.typescript-intro/2.pre-assignment/src/2.ts
+++ w1/1.typescript-intro/2.pre-assignment/src/2.ts
@@ -1,1 +1,12 @@
 // 2. Create a type age with a number ?
+
+
+
+let age: number = 51;
+
+age = 89; // success (okay)
+
+age = "fifty one";   // error (not okay)
+
+
+

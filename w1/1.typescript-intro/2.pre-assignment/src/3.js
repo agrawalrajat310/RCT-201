d188bb515104569ae0e6574ade72bc619d87e991
agrawalrajat310 <agrawalrajat03@outlook.com>
2022-12-25 01:32:52 +0530

    day - 1 assign push

---
+++ w1/1.typescript-intro/2.pre-assignment/src/3.js
@@ -0,0 +1,4 @@
+// 3. Create a type isFetching with boolean ?
+var isFetching = true;
+isFetching = false;
+isFetching = "false"; // error (not okay)
--- w1/1.typescript-intro/2.pre-assignment/src/3.ts
+++ w1/1.typescript-intro/2.pre-assignment/src/3.ts
@@ -1,1 +1,10 @@
 // 3. Create a type isFetching with boolean ?
+
+
+
+
+let isFetching: boolean = true;
+
+isFetching = false;
+
+isFetching = "false"; // error (not okay)

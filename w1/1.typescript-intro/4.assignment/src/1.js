84c406a6e1655e64b37bd62bfc33ce073bce1a06
agrawalrajat310 <agrawalrajat03@outlook.com>
2022-12-25 19:28:53 +0530

    day - 1 assign push

--- w1/1.typescript-intro/4.assignment/src/1.js
+++ w1/1.typescript-intro/4.assignment/src/1.js
@@ -1,12 +1,6 @@
 // Create a function that takes 2 arguments, x, y both numbers ?
-
 // it should return the product of the 2 numbers
-
-
-const mul = (a: number, b: number): number => {
-    return a*b;
+var mul = function (a, b) {
+    return a * b;
 };
-
-mul(2,4);  // 8
-
-
+mul(2, 4); // 8

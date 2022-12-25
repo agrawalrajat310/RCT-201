202c23c260a4a15f857acc224659ddc06ab08ed4
agrawalrajat310 <agrawalrajat03@outlook.com>
2022-12-25 14:55:30 +0530

    day - 1 assign push

---
+++ w1/1.typescript-intro/2.pre-assignment/src/5.js
@@ -0,0 +1,3 @@
+// 5. Create an array of strings ?
+var arr = ["A", "B", "C"]; // (okay)
+arr.push("G");
--- w1/1.typescript-intro/2.pre-assignment/src/5.ts
+++ w1/1.typescript-intro/2.pre-assignment/src/5.ts
@@ -1,1 +1,8 @@
 // 5. Create an array of strings ?
+
+
+
+let arr:Array<string> = ["A", "B", "C"];  // (okay)
+
+arr.push("G");
+
